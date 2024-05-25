e8bd8e86256ac0e2f2a7777f6b6ec6a991a27829
geniusyinka <yunggenius4@gmail.com>
2024-05-25 13:22:53 +0100

    vue template updates

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <div class="hero-top">
-      <img src='@/assets/fleekLogo.svg' height="87" />
-      <img src='@/assets/plus.svg' />
-      <img src='@/assets/logo.svg' height="57" />
-    </div>
-
-    <p className='description'>
-      This is a template for creating a React site build it with Vite and deploying it on Fleek.
-    </p>
-  </header>
-
-  <RouterView />
+  <img id="img" alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<style scoped>
-template {
-  display: flex;
-  flex-direction: column;
-}
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-header {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 2.5rem;
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
+</script>
 
-.hero-top {
-  display: flex;
-  align-items: center;
-  gap: 50px;
-  margin-left: -95px;
-}
-
-.description {
-  font-size: 1.5rem;
-  line-height: 2rem;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  width: 100%;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#img {
+  width: 200px;
+    height: 200px;
+    margin: 0 auto;
 }
 </style>
